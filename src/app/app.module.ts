ce7d7e7497c523cf2875ab43ead410c645a4b339
Le Feng <lfung218@Les-MacBook-Air.local>
2018-02-21 18:45:50 -0500

    Added Routing Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,27 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 import { BookDetailComponent } from './book-detail/book-detail.component';
+import { HomeComponent } from './home/home.component';
+import { OrdersComponent } from './orders/orders.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRouterModule } from './/router.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksComponent,
-    BookDetailComponent
+    BookDetailComponent,
+    HomeComponent,
+    OrdersComponent,
+    NavBarComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
